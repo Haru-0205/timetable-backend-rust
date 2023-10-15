56c356fef926cc54baaad2e45d8ca4551552f9c3
haru-0205 <rm23081b@osaka-pct.ac.jp>
2023-10-15 23:06:22 +0900

    build(deps): update settings.json, Cargo.lock, Cargo.toml and main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello,World!")
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    "Hello,World"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(web::scope("/app").route("index.html", web::get().to(index)))
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
