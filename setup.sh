4f73ee4197c2855543422b09897f6dd094114bbf
haru-0205 <rm23081b@osaka-pct.ac.jp>
2023-10-28 10:08:33 +0900

    update 3 files and create 1 file

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# git clone https://github.com/haru-0205/timetable-frontend-next
+echo "メールアドレスを入力してください"
+read email
+git config --global user.email $email
+echo "ユーザー名を入力してください"
+read name
+git config --global user.name $name
+gh auth login
+git fetch
+git pull
+git checkout develop
