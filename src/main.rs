3de0b0ec79d6ea4b0b42ca1eefe8b7986c62cb4b
haru-0205 <rm23081b@osaka-pct.ac.jp>
2023-11-02 11:49:55 +0000

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 
-#[get("/")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().json("{\"message\":\"JSON Test\"}")
+#[derive(Debug, Serialize, Deserialize)]
+struct Todo {
+    id: i32,
+    content: String,
+    checked: bool,
+}
+
+#[get("/api/todo")]
+async fn todo_index() -> impl Responder {
+    HttpResponse::Ok().json(Todo {
+        id: 1,
+        content: "やることはapi".to_string(),
+        checked: false,
+    })
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind(("localhost", 8080))?
+    HttpServer::new(|| App::new().service(todo_index))
+        .bind(("web", 8080))?
         .run()
         .await
 }
